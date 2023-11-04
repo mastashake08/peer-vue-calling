635342f2142381c5324c83f3eaff6c3515de3ef4
Hacker God <jyrone.parker@gmail.com>
2023-11-04 19:27:02 -0400

    maps

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,24 @@
   <div class="hello">
     <h1>{{ connectionText }}</h1>
     <input id="channel-id" type="text" v-model="channelId" />
-    <button @click="startCall" id="call-button">Call Channel</button>
+    <div class="buttons">
+      <button @click="startCall" id="call-button">Call Channel</button>
+      <button @click="sendLocation" id="location-button">Send Location</button>
+    </div>
+
+    <p id="status"></p>
+    <a id="map-link" target="_blank"></a>
+    <div id="map"  style="height: 100%; width: 100%"></div>
   </div>
 </template>
 
 <script>
 import { Peer } from 'peerjs'
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM.js';
+import {useGeographic} from 'ol/proj'
 export default {
   name: 'HelloWorld',
   props: {
@@ -22,7 +34,8 @@ export default {
       remoteAudio: null,
       remoteStream: null,
       connectionText: 'Not connected. Start a call!',
-      connection: null
+      connection: null,
+      map: {}
     }
   },
   created() {
@@ -31,7 +44,33 @@ export default {
       this.connectionText = `Your device ID is: ${this.peer.id}`;
     });
     this.peer.on('connection', (connection) => {
+      console.log(connection)
       this.connection = connection
+      this.connection.on('data', (data) => {
+        
+        const status = document.querySelector("#status");
+        const mapLink = document.querySelector("#map-link");
+        const latitude = data.latitude;
+        const longitude = data.longitude;
+        useGeographic()
+        console.log(data)
+        const map = new Map({
+          target: 'map',
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+          ],
+          view: new View({
+            center: [latitude, longitude],
+            zoom: 2
+          })
+        });
+        console.log(map)
+        status.textContent = "";
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+      })
     
     })
     this.peer.on("call", (call) => {
@@ -53,14 +92,29 @@ export default {
   mounted () {
     this.localAudio = new Audio()
     this.remoteAudio = new Audio()
+
   },
   methods: {
+    sendLocation(){
+      const status = document.querySelector("#status");
+      if (!navigator.geolocation) {
+        status.textContent = "Geolocation is not supported by your browser";
+      } else {
+        status.textContent = "Locating…";
+        navigator.geolocation.getCurrentPosition(this.success, this.error, {maximumAge:60000, timeout:5000, enableHighAccuracy:true});
+      }
+    },
+    success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      this.connection.send({latitude: latitude, longitude: longitude})
+    },
     async startCall() {
       console.log('Starting Call')
       await this.getLocalStream()
       this.connectPeers()
       const call = this.peer.call(this.channelId, this.localStream); // A
-      console.log(call)
+      
       call.on("stream", (stream) => {
         // B
         this.remoteAudio.srcObject = stream; // C
@@ -70,7 +124,32 @@ export default {
 
     },
     connectPeers() {
-      this.peer.connect(this.channelId)
+      this.connection = this.peer.connect(this.channelId)
+      this.connection.on('data', (data) => {
+        console.log(data)
+        const status = document.querySelector("#status");
+        const mapLink = document.querySelector("#map-link");
+        const latitude = data.latitude;
+        const longitude = data.longitude;
+        useGeographic()
+        
+        const map = new Map({
+          target: 'map',
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+          ],
+          view: new View({
+            center: [latitude, longitude],
+            zoom: 2
+          })
+        });
+        console.log(map)
+        status.textContent = "";
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+      })
     },
     async getLocalStream() {
       try {
@@ -105,5 +184,14 @@ li {
 }
 a {
   color: #42b983;
+}
+html, body {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+}
+#map {
+  height: 100%;
+  width: 100%;
 }
 </style>
